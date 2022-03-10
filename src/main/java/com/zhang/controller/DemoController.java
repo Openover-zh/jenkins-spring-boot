fd352c76614fab8659d718db5ad1608c4c60f687
Jia <1589391440@qq.com>
2022-03-10 14:28:12 +0800

    hello jenkins

---
+++ src/main/java/com/zhang/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.zhang.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class DemoController {
+    @GetMapping
+    public String test(){
+        return "this is my jenkins!!";
+
+    }
+
+}
